25fabddf9a1eb823b666102e996c1f6eabe4bdb6
gabrie-lhilarion <gabrielhilarion@gmail.com>
2024-05-23 07:01:21 +0100

    create auth template

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 from dotenv import load_dotenv
 import os
 
@@ -7,6 +10,12 @@ import os
 load_dotenv()
 
 db = SQLAlchemy()
+
+migrate = Migrate()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+login_manager.login_message_category = 'info'
 
 def create_app():
     app = Flask(__name__)
@@ -16,6 +25,9 @@ def create_app():
 
     # Initialize extensions
     db.init_app(app)
+    migrate.init_app(app, db)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
 
     # Register blueprints
     from .main import bp as main_bp
@@ -25,4 +37,12 @@ def create_app():
     from .create_db import create_database
     create_database()
 
+    from app.auth import auth as auth_bp
+    app.register_blueprint(auth_bp)
+
     return app
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
